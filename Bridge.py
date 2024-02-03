0b0827937c0aac68bff1f147734e9fae23ee71cc
LAPTOP-HQCC98K4\Jihoon <hoijon2210@gmail.com>
2024-02-03 18:53:09 +0900

    #main - setting

---
+++ Bridge.py
@@ -0,0 +1,13 @@
+from Date import date_select
+from Link import link_go
+from Login import login
+from Main import Setting
+from Payment import payment
+from Seat import seat_select
+
+class Setting():
+    def __init__(self):
+        
+
+#Login
+
--- Main.py
+++ Main.py
@@ -1,12 +1,21 @@
+from UI import Interface
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 import threading
+
 class App(threading.Thread):
-    super().__init__()
-    def run_multiple_commands(self):
-        # 여러 함수를 순차적으로 호출
-        self.login()
-        self.link_go()
-        self.seat_select()
-        self.payment()
+    def __init__(self):
+        super().__init__()
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+        self.url = "https://ticket.interpark.com/Gate/TPLogin.asp"
+        self.driver.get(self.url)
+        
+        Interface()
 
 app = App()
 app.start()
